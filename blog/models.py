5fbbe8b169ee6b0879938769321a915196583f06
aeashkan <160534061+aeashkan@users.noreply.github.com>
2024-05-27 19:02:17 +0330

    new changes
    add author
    post title

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Post(models.Model):
-
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     counted_view = models.IntegerField(default=0)
@@ -15,4 +17,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    class Meta:
+        ordering = ['-created_date']
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from blog.models import Post
+
 
 def blog_view(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog.html', context)
 
-def single_view(request):
-    return render(request, 'blog/single.html')
+
+def single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid, status=1)
+    context = {'post': post}
+    return render(request, 'blog/single.html', context)
